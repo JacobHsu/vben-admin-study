c9a6a9e35b506b2dcc94801b373d1f7810119e0d
jacobhsu <jacob.hsu.tw@gmail.com>
2022-02-08 17:57:54 +0800

    feat: config paths /@/ /#/

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { initAppConfigStore } from '/@/logics/initAppConfig';
 
-createApp(App).mount('#app')
+function bootstrap() {
+  // createApp(App).mount('#app')
+  const app = createApp(App);
+
+  // Initialize internal system configuration
+  initAppConfigStore();
+
+  app.mount('#app');
+}
+
+bootstrap();
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
+import type { UserConfig, ConfigEnv } from 'vite';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { loadEnv } from 'vite';
+import { resolve } from 'path';
+import { wrapperEnv } from './build/utils';
+
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir);
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+  const root = process.cwd();
+  const env = loadEnv(mode, root);
+
+  // The boolean type read by loadEnv is a string. This function can be converted to boolean type
+  const viteEnv = wrapperEnv(env);
+
+  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_DROP_CONSOLE } = viteEnv;
+
+  return {
+    base: VITE_PUBLIC_PATH,
+    root,
+    resolve: {
+      alias: [
+        {
+          find: 'vue-i18n',
+          replacement: 'vue-i18n/dist/vue-i18n.cjs.js',
+        },
+        // /@/xxxx => src/xxxx
+        {
+          find: /\/@\//,
+          replacement: pathResolve('src') + '/',
+        },
+        // /#/xxxx => types/xxxx
+        {
+          find: /\/#\//,
+          replacement: pathResolve('types') + '/',
+        },
+      ],
+    },
+    plugins: [vue()]
+  }
+}
