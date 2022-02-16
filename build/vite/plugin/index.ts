1231b2ba4a2dbe8c9781413bc124a760d781cb3a
jacobhsu <jacob.hsu.tw@gmail.com>
2022-02-16 15:51:57 +0800

    feat: build createVitePlugins theme.ts

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -2,6 +2,7 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import VitePluginCertificate from 'vite-plugin-mkcert';
 import vueSetupExtend from 'vite-plugin-vue-setup-extend';
+import { configThemePlugin } from './theme';
 
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   const vitePlugins = [
@@ -15,6 +16,9 @@ export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
       source: 'coding',
     }),
   ];
+
+  // vite-plugin-theme
+  // vitePlugins.push(configThemePlugin(isBuild));
 
   return vitePlugins;
 }
