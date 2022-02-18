0c4d5b08c4753e82bb0ae40dd354e34dcdd84a35
jacobhsu <jacob.hsu.tw@gmail.com>
2022-02-18 14:41:35 +0800

    feat: main.ts setupI18n vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { initAppConfigStore } from '/@/logics/initAppConfig';
+import { setupI18n } from '/@/locales/setupI18n';
 
-function bootstrap() {
+async function bootstrap() {
   // createApp(App).mount('#app')
   const app = createApp(App);
 
   // Initialize internal system configuration
   initAppConfigStore();
+
+  // Multilingual configuration
+  // Asynchronous case: language files may be obtained from the server side
+  await setupI18n(app);
 
   app.mount('#app');
 }
