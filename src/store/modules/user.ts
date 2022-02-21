9b2c595d54755ff692be0fe3d3fb3091332619ca
jacobhsu <jacob.hsu.tw@gmail.com>
2022-02-21 17:50:16 +0800

    feat: views LockPage

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,23 +1,34 @@
+import type { UserInfo } from '/#/store';
 import { defineStore } from 'pinia';
 import { store } from '/@/store';
+import { PageEnum } from '/@/enums/pageEnum';
 import { ROLES_KEY, TOKEN_KEY, USER_INFO_KEY } from '/@/enums/cacheEnum';
 import { getAuthCache, setAuthCache } from '/@/utils/auth';
-
+import { router } from '/@/router';
 
 interface UserState {
+  userInfo: Nullable<UserInfo>;
   token?: string;
   sessionTimeout?: boolean;
+  lastUpdateTime: number;
 }
 
 export const useUserStore = defineStore({
   id: 'app-user',
   state: (): UserState => ({
+    // user info
+    userInfo: null,
     // token
     token: undefined,
     // Whether the login expired
     sessionTimeout: false,
+    // Last fetch time
+    lastUpdateTime: 0,
   }),
   getters: {
+    getUserInfo(): UserInfo {
+      return this.userInfo || getAuthCache<UserInfo>(USER_INFO_KEY) || {};
+    },
     getToken(): string {
       return this.token || getAuthCache<string>(TOKEN_KEY);
     },
@@ -29,6 +40,11 @@ export const useUserStore = defineStore({
     setToken(info: string | undefined) {
       this.token = info ? info : ''; // for null or undefined value
       setAuthCache(TOKEN_KEY, info);
+    },
+    setUserInfo(info: UserInfo | null) {
+      this.userInfo = info;
+      this.lastUpdateTime = new Date().getTime();
+      setAuthCache(USER_INFO_KEY, info);
     },
     setSessionTimeout(flag: boolean) {
       this.sessionTimeout = flag;
--- src/views/sys/lock/index.vue
+++ src/views/sys/lock/index.vue
@@ -1,11 +1,12 @@
 <template>
   <transition name="fade-bottom" mode="out-in">
-    LockPage
+    <!-- <LockPage v-if="getIsLock" /> -->
+    <LockPage />
   </transition>
 </template>
 <script lang="ts" setup>
   import { computed } from 'vue';
-
+  import LockPage from './LockPage.vue';
   import { useLockStore } from '/@/store/modules/lock';
 
   const lockStore = useLockStore();
