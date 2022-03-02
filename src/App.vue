af930aa144ff7e00cb146bc7866ed9f3d8828e35
jacobhsu <jacob.hsu.tw@gmail.com>
2022-03-02 17:53:32 +0800

    fix: remove App #app style

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,6 @@
   <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
-
 <script setup lang="ts">
 import { ConfigProvider } from 'ant-design-vue';
 import { AppProvider } from '/@/components/Application';
