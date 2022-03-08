401b593bc9fc138958cc95dbdda585396a44c9b0
jacobhsu <jacob.hsu.tw@gmail.com>
2022-03-08 17:55:58 +0800

    chore: setupRouterGuard createPageLoadingGuard

--- src/router/guard/index.ts
+++ src/router/guard/index.ts
@@ -1,9 +1,14 @@
 import type { Router, RouteLocationNormalized } from 'vue-router';
+import { useAppStoreWithOut } from '/@/store/modules/app';
+import { useUserStoreWithOut } from '/@/store/modules/user';
+import { useTransitionSetting } from '/@/hooks/setting/useTransitionSetting';
 import { setRouteChange } from '/@/logics/mitt/routeChange';
+import { unref } from 'vue';
 
 // Don't change the order of creation
 export function setupRouterGuard(router: Router) {
   createPageGuard(router);
+  createPageLoadingGuard(router);
 }
 
 /**
@@ -23,5 +28,37 @@ function createPageGuard(router: Router) {
 
   router.afterEach((to) => {
     loadedPageMap.set(to.path, true);
+  });
+}
+
+// Used to handle page loading status
+function createPageLoadingGuard(router: Router) {
+  const userStore = useUserStoreWithOut();
+  const appStore = useAppStoreWithOut();
+  const { getOpenPageLoading } = useTransitionSetting();
+  router.beforeEach(async (to) => {
+    if (!userStore.getToken) {
+      return true;
+    }
+    if (to.meta.loaded) {
+      return true;
+    }
+
+    if (unref(getOpenPageLoading)) {
+      appStore.setPageLoadingAction(true);
+      return true;
+    }
+
+    return true;
+  });
+  router.afterEach(async () => {
+    if (unref(getOpenPageLoading)) {
+      // TODO Looking for a better way
+      // The timer simulates the loading time to prevent flashing too fast,
+      setTimeout(() => {
+        appStore.setPageLoading(false);
+      }, 220);
+    }
+    return true;
   });
 }
