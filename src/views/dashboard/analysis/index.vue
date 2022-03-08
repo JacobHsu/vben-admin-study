be14dafcfa874b5e1bc873ad890fccc85ae10d67
jacobhsu <jacob.hsu.tw@gmail.com>
2022-03-08 14:54:09 +0800

    feat: dashboard SiteAnalysis

--- src/views/dashboard/analysis/index.vue
+++ src/views/dashboard/analysis/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="p-4">
     <GrowCard :loading="loading" class="enter-y" />
+    <SiteAnalysis class="!my-4 enter-y" :loading="loading" />
   </div>
 </template>
 <script lang="ts" setup>
   import { ref } from 'vue';
   import GrowCard from './components/GrowCard.vue';
+  import SiteAnalysis from './components/SiteAnalysis.vue';
   const loading = ref(true);
 
   setTimeout(() => {
