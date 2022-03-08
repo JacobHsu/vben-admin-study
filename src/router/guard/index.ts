eaa1ea6ed41ce9d9014450bfa769cc918ad0a190
jacobhsu <jacob.hsu.tw@gmail.com>
2022-03-08 18:03:59 +0800

    chore: setupRouterGuard createHttpGuard createScrollGuard

--- src/router/guard/index.ts
+++ src/router/guard/index.ts
@@ -2,13 +2,17 @@ import type { Router, RouteLocationNormalized } from 'vue-router';
 import { useAppStoreWithOut } from '/@/store/modules/app';
 import { useUserStoreWithOut } from '/@/store/modules/user';
 import { useTransitionSetting } from '/@/hooks/setting/useTransitionSetting';
+import { AxiosCanceler } from '/@/utils/http/axios/axiosCancel';
 import { setRouteChange } from '/@/logics/mitt/routeChange';
 import { unref } from 'vue';
+import projectSetting from '/@/settings/projectSetting';
 
 // Don't change the order of creation
 export function setupRouterGuard(router: Router) {
   createPageGuard(router);
   createPageLoadingGuard(router);
+  createHttpGuard(router);
+  createScrollGuard(router);
 }
 
 /**
@@ -59,6 +63,38 @@ function createPageLoadingGuard(router: Router) {
         appStore.setPageLoading(false);
       }, 220);
     }
+    return true;
+  });
+}
+
+/**
+ * The interface used to close the current page to complete the request when the route is switched
+ * @param router
+ */
+function createHttpGuard(router: Router) {
+  const { removeAllHttpPending } = projectSetting;
+  let axiosCanceler: Nullable<AxiosCanceler>;
+  if (removeAllHttpPending) {
+    axiosCanceler = new AxiosCanceler();
+  }
+  router.beforeEach(async () => {
+    // Switching the route will delete the previous request
+    axiosCanceler?.removeAllPending();
+    return true;
+  });
+}
+
+// Routing switch back to the top
+function createScrollGuard(router: Router) {
+  const isHash = (href: string) => {
+    return /^#/.test(href);
+  };
+
+  const body = document.body;
+
+  router.afterEach(async (to) => {
+    // scroll top
+    isHash((to as RouteLocationNormalized & { href: string })?.href) && body.scrollTo(0, 0);
     return true;
   });
 }
