6a027d13164fef875ee75bcc726365a1814e0cee
jacobhsu <jacob.hsu.tw@gmail.com>
2022-03-08 14:25:58 +0800

    feat: dashboard GrowCard

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -3,6 +3,7 @@ import type { AppRouteRecordRaw, AppRouteModule } from '/@/router/types';
 import { PAGE_NOT_FOUND_ROUTE } from '/@/router/routes/basic';
 
 import { PageEnum } from '/@/enums/pageEnum';
+import { t } from '/@/hooks/web/useI18n';
 
 export const asyncRoutes = [PAGE_NOT_FOUND_ROUTE];
 
@@ -15,9 +16,29 @@ export const RootRoute: AppRouteRecordRaw = {
   },
 };
 
+export const LoginRoute: AppRouteRecordRaw = {
+  path: '/login',
+  name: 'Login',
+  component: () => import('/@/views/sys/login/Login.vue'),
+  meta: {
+    title: t('routes.basic.login'),
+  },
+};
+
+export const DashboardRoute: AppRouteRecordRaw = {
+  path: '/dashboard',
+  name: 'Dashboard',
+  component: () => import('/@/views/dashboard/analysis/index.vue'),
+  meta: {
+    title: 'Dashboard',
+  },
+};
+
+
 // Basic routing without permission
 export const basicRoutes = [
-  // LoginRoute,
+  DashboardRoute,
+  LoginRoute,
   RootRoute,
   // ...mainOutRoutes,
   // REDIRECT_ROUTE,
---
+++ src/views/dashboard/analysis/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="p-4">
+    <GrowCard :loading="loading" class="enter-y" />
+  </div>
+</template>
+<script lang="ts" setup>
+  import { ref } from 'vue';
+  import GrowCard from './components/GrowCard.vue';
+  const loading = ref(true);
+
+  setTimeout(() => {
+    loading.value = false;
+  }, 1500);
+</script>
