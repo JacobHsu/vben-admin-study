8a10f81913764a8ecb4e9de48d9637e6e288e388
jacobhsu <jacob.hsu.tw@gmail.com>
2022-04-29 15:27:35 +0800

    feat: LayoutSideBar layoutClass

--- src/layouts/default/index.vue
+++ src/layouts/default/index.vue
@@ -2,6 +2,9 @@
   <Layout :class="prefixCls" v-bind="lockEvents">
     <!-- <LayoutFeatures /> -->
     <LayoutHeader fixed  />
+    <Layout :class="[layoutClass]">
+        <LayoutSideBar />
+    </Layout>
   </Layout>
 </template>
 
@@ -10,8 +13,10 @@
   import { Layout } from 'ant-design-vue';
 
   import LayoutHeader from './header/index.vue';
+  import LayoutSideBar from './sider/index.vue';
   import { createAsyncComponent } from '/@/utils/factory/createAsyncComponent';
   import { useHeaderSetting } from '/@/hooks/setting/useHeaderSetting';
+  import { useMenuSetting } from '/@/hooks/setting/useMenuSetting';
   import { useDesign } from '/@/hooks/web/useDesign';
   import { useLockPage } from '/@/hooks/web/useLockPage';
   
@@ -20,19 +25,30 @@
     components: {
       LayoutFeatures: createAsyncComponent(() => import('/@/layouts/default/feature/index.vue')),
       LayoutHeader,
+      LayoutSideBar,
       Layout,
     },
     setup() {
       const { prefixCls } = useDesign('default-layout');
       const { getShowFullHeaderRef } = useHeaderSetting();
+      const { getIsMixSidebar, getShowMenu } = useMenuSetting();
 
       // Create a lock screen monitor
       const lockEvents = useLockPage();
+
+      const layoutClass = computed(() => {
+        let cls: string[] = ['ant-layout'];
+        if (unref(getIsMixSidebar) || unref(getShowMenu)) {
+          cls.push('ant-layout-has-sider');
+        }
+        return cls;
+      });
 
       return {
         getShowFullHeaderRef,
         prefixCls,
         lockEvents,
+        layoutClass,
       };
     },
   });
